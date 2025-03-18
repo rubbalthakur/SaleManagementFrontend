a647af71eb0efc4e60c35053b09013036e9b4939
rubbal123 <rubbalblockcoders@gmail.com>
2025-03-18 15:05:21 +0530

    added tabs in settings

--- app/components/screens/Settings/Settings.tsx
+++ app/components/screens/Settings/Settings.tsx
@@ -1,45 +1,45 @@
 "use client";
 import { useState } from "react";
 export function Settings() {
-  const [activeTab, setActiveTab] = useState("profile");
+  const [activeSetting, setActiveSetting] = useState("profile");
   return (
     <div className="p-6 space-y-6 text-black">
       <button
-        onClick={() => setActiveTab("profile")}
+        onClick={() => setActiveSetting("profile")}
         style={{ border: "1px solid black", margin: "auto 5px" }}
       >
         Profile
       </button>
       <button
-        onClick={() => setActiveTab("changePassword")}
+        onClick={() => setActiveSetting("changePassword")}
         style={{ border: "1px solid black", margin: "auto 5px" }}
       >
         Change Password
       </button>
       <button
-        onClick={() => setActiveTab("users")}
+        onClick={() => setActiveSetting("users")}
         style={{ border: "1px solid black", margin: "auto 5px" }}
       >
         Users
       </button>
       <button
-        onClick={() => setActiveTab("leadTypes")}
+        onClick={() => setActiveSetting("leadTypes")}
         style={{ border: "1px solid black", margin: "auto 5px" }}
       >
         Lead Types
       </button>
       <button
-        onClick={() => setActiveTab("leadSources")}
+        onClick={() => setActiveSetting("leadSources")}
         style={{ border: "1px solid black", margin: "auto 5px" }}
       >
         LeadSources
       </button>
       <div className="flex-1 overflow-auto p-6">
-        {activeTab === "profile" && <div>profile abcd</div>}
-        {activeTab === "changePassword" && <div>changePassword abc</div>}
-        {activeTab === "users" && <div>Users abc</div>}
-        {activeTab === "leadTypes" && <div>Lead Types abc</div>}
-        {activeTab === "leadSources" && <div>Lead Sources abc</div>}
+        {activeSetting === "profile" && <div>profile abcd</div>}
+        {activeSetting === "changePassword" && <div>changePassword abc</div>}
+        {activeSetting === "users" && <div>Users abc</div>}
+        {activeSetting === "leadTypes" && <div>Lead Types abc</div>}
+        {activeSetting === "leadSources" && <div>Lead Sources abc</div>}
       </div>
     </div>
   );
